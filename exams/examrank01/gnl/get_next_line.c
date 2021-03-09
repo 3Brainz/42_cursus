61a1dd122aeec84fcc3d6febaacfd56e6c32eb9c
Paolo roberto Punzo <ppunzo@e4r2p1.42roma.it>
2021-03-09 13:56:55 +0100

    gnl

--- exams/examrank01/gnl/get_next_line.c
+++ exams/examrank01/gnl/get_next_line.c
@@ -10,9 +10,85 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+	int index;
+	
+	index = 0;
+	while (str[index])
+	{
+		index++;
+	}
+	return (index);
+}
+/*
+int get_next_line(int fd, char **line)
+{
+	size_t	buff_index = 0;
+	int red;
+	char buffer[1024*1024];
+	
+	if (!line)
+		return (-1);
+	while ((red = read(fd, &buffer[buff_index], 1)) >= 0)
+	{
+		if (buffer[buff_index] == '\n' || red == 0)
+		{
+			buffer[buff_index] = 0;
+			*line = (char *)malloc(sizeof(char) * (ft_strlen(buffer) + 1));
+			if (!*line)
+				return (-1);
+			buff_index = 0;
+			while (buffer[buff_index])
+			{
+				(*line)[buff_index] = buffer[buff_index];
+				buffer[buff_index] = 0;
+				buff_index++;
+			}
+			(*line)[buff_index] = 0;
+			return(red == 0 ? 0 : 1);
+		}
+		else
+			buff_index++;
+	}
+	return (-1);
+}
+*/
 int get_next_line(char **line)
 {
-	char buffer[1024*1024];
-
+	char	buffer[1024*1024];
+	int		red;
+	size_t	buff_index;
 	
+	if (!line)
+		return (-1);
+	while ((red = read(0, &buffer[buff_index], 1)) >= 0)
+	{
+		if (buffer[buff_index] == '\n' || red == 0)
+		{
+			buffer[buff_index] = 0;
+			*line = (char *)malloc(sizeof(char) * (ft_strlen(buffer) + 1));
+			if (!*line)
+				return (-1);
+			buff_index = 0;
+			while (buffer[buff_index])
+			{
+				line[0][buff_index] = buffer[buff_index];
+				buffer[buff_index] = 0;
+				buff_index++;
+			}
+			line[0][buff_index] = 0;
+			if (red == 0)
+				return (0);
+			else
+				return (1);
+		}
+		else
+			buff_index++;
+	}
+	return (-1);
 }
---
+++ exams/examrank01/gnl/get_next_line.h
@@ -0,0 +1,7 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+int get_next_line(int fd, char **line);
+int ft_strlen(char *str);
+
+#endif
